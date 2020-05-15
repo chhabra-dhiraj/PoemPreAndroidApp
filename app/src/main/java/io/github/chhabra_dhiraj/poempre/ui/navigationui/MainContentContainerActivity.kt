c33aab51365b9e0e816fe41341864cd76dd751db
DHIRAJ CHHABRA <dhirajchhabra630@gmail.com>
2020-05-16 00:26:25 +0530

    added code for splash screen

--- app/src/main/java/io/github/chhabra_dhiraj/poempre/ui/navigationui/MainContentContainerActivity.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/poempre/ui/navigationui/MainContentContainerActivity.kt
@@ -7,6 +7,7 @@ import io.github.chhabra_dhiraj.poempre.R
 class MainContentContainerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
