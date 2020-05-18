4d1b548075f0cc57986db4c387380d77924db3e1
DHIRAJ CHHABRA <dhirajchhabra630@gmail.com>
2020-05-18 19:11:29 +0530

    added all the navigation graphs of the app

---
+++ app/src/main/java/io/github/chhabra_dhiraj/poempre/ui/main/authentication/UserDetailsInputScreen.kt
@@ -0,0 +1,23 @@
+package io.github.chhabra_dhiraj.poempre.ui.main.authentication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.chhabra_dhiraj.poempre.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class UserDetailsInputScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_details_input_screen, container, false)
+    }
+
+}
---
+++ app/src/main/java/io/github/chhabra_dhiraj/poempre/ui/main/maincontent/home/ViewPoemScreenFragment.kt
@@ -0,0 +1,23 @@
+package io.github.chhabra_dhiraj.poempre.ui.main.maincontent.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import io.github.chhabra_dhiraj.poempre.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ViewPoemScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_poem_screen, container, false)
+    }
+
+}
